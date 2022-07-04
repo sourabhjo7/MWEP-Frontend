4c3c3571a18627bc42204e904de87285cac817b0
sourabhjo7 <sourabhjoshi9549408165@gmail.com>
2022-07-04 19:32:38 +0530

    code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-     react 
-    </div>
+    <Routes>
+      
+    <Route path="/" element={<Home />} />
+
+    </Routes>
+  
   );
 }
 
